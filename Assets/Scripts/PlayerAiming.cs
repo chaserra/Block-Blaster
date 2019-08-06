c6e50a175162c6bf47d32dca5145ca8d88960416
Charles Serrato <serrato.charles@gmail.com>
2019-08-06 16:07:39 +0800

    Basic Aiming added

---
+++ Assets/Scripts/PlayerAiming.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerAiming : MonoBehaviour {
+
+    //Cache
+    [SerializeField] Transform turret;
+
+    //Parameters
+    [SerializeField] float speed = 100f;
+
+    void Start() {
+        
+    }
+
+
+    void Update() {
+        RotateTurret();
+    }
+
+    private void RotateTurret() {
+        if(CrossPlatformInputManager.GetButton("Fire1")) {
+            float xRotate = CrossPlatformInputManager.GetAxis("Mouse Y") * speed * Time.deltaTime;
+            float yRotate = CrossPlatformInputManager.GetAxis("Mouse X") * speed * Time.deltaTime;
+            turret.transform.Rotate(-xRotate, yRotate, 0);
+
+            float clampedX = turret.transform.localEulerAngles.x;
+            clampedX = Mathf.Clamp((clampedX <= 180) ? clampedX : -(360 - clampedX), -45f, -11f);
+            float clampedY = turret.transform.localEulerAngles.y;
+            clampedY = Mathf.Clamp((clampedY <= 180) ? clampedY : -(360 - clampedY), -30f, 30f);
+
+            turret.transform.rotation = Quaternion.Euler(clampedX, clampedY, 0);
+        }
+    }
+}
