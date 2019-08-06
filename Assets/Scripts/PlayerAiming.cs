935cd0146daf07bbfa97dec6e8446f2cca501988
Charles Serrato <serrato.charles@gmail.com>
2019-08-06 18:00:58 +0800

    Shooting Updated: +Projectile trajectory enabled +Shoot cooldown enabled

--- Assets/Scripts/PlayerAiming.cs
+++ Assets/Scripts/PlayerAiming.cs
@@ -8,9 +8,16 @@ public class PlayerAiming : MonoBehaviour {
 
     //Cache
     [SerializeField] Transform turret;
+    [SerializeField] GameObject firePoint;
 
     //Parameters
-    [SerializeField] float speed = 100f;
+    [SerializeField] GameObject tankBullet;
+    [SerializeField] float rotateSpeed = 150f;
+    [Tooltip("Higher value means faster cooldown.")]
+    [SerializeField] float rateOfFire = .6f;
+
+    //State
+    private float timeToFire = 0;
 
     void Start() {
         
@@ -18,21 +25,34 @@ public class PlayerAiming : MonoBehaviour {
 
 
     void Update() {
-        RotateTurret();
+        RotateAndShoot();
     }
 
-    private void RotateTurret() {
+    private void RotateAndShoot() {
         if(CrossPlatformInputManager.GetButton("Fire1")) {
-            float xRotate = CrossPlatformInputManager.GetAxis("Mouse Y") * speed * Time.deltaTime;
-            float yRotate = CrossPlatformInputManager.GetAxis("Mouse X") * speed * Time.deltaTime;
+            float xRotate = CrossPlatformInputManager.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+            float yRotate = CrossPlatformInputManager.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
             turret.transform.Rotate(-xRotate, yRotate, 0);
 
             float clampedX = turret.transform.localEulerAngles.x;
-            clampedX = Mathf.Clamp((clampedX <= 180) ? clampedX : -(360 - clampedX), -45f, -11f);
+            clampedX = Mathf.Clamp((clampedX <= 180) ? clampedX : -(360 - clampedX), -45f, -3f);
             float clampedY = turret.transform.localEulerAngles.y;
-            clampedY = Mathf.Clamp((clampedY <= 180) ? clampedY : -(360 - clampedY), -30f, 30f);
+            clampedY = Mathf.Clamp((clampedY <= 180) ? clampedY : -(360 - clampedY), -45f, 45f);
 
             turret.transform.rotation = Quaternion.Euler(clampedX, clampedY, 0);
+        }
+
+        if(CrossPlatformInputManager.GetButtonUp("Fire1")) {
+            if (Time.time >= timeToFire) {
+                timeToFire = Time.time + 1 / rateOfFire;
+                //TODO: Low-prio: Add cooldown bar UI
+                GameObject bullet = Instantiate(tankBullet, firePoint.transform.position, Quaternion.identity);
+                bullet.transform.localRotation = turret.rotation;
+                Destroy(bullet, 10f);
+                //TODO: Med-prio: Add turret retracting animation (for impact visuals)
+            } else {
+                Debug.Log("Can't fire yet!"); //TODO: Low-prio: Remove or add sound effect
+            }
         }
     }
 }
---
+++ Assets/Scripts/ProjectileMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour {
+
+    //Cache
+    private Rigidbody rb;
+
+    //Parameters
+    [SerializeField] float speed = 2f;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        ShootProjectile();
+    }
+
+    private void ShootProjectile() {
+        if (speed != 0) {
+            rb.AddForce(transform.forward * speed, ForceMode.Impulse);
+        } else {
+            Debug.LogWarning("No speed added on projectile");
+        }
+    }
+
+}
